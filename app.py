bf847ac285befe3824bbb901b42f11ed08efd83b
oowo1 <bujupara12@gmail.com>
2023-02-02 19:49:32 -0700

    First python commit

---
+++ app.py
@@ -0,0 +1,11 @@
+first_name = "John"
+last_name = "Smith"
+age = 20
+is_new = False
+print(first_name)
+print(last_name)
+print(age)
+if is_new is True:
+    print ("New Patient")
+if is_new is False:
+    print ("Existing patient")
